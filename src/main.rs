fb6da79f41c0b3a792a7d1d0e678567318440c37
tomasnyberg <tomas.nyberg7335@gmail.com>
2024-12-22 11:28:17 +0100

    main: add repl

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,22 @@
 use chunk::Chunk;
+use std::io::{self, Write};
 
 mod chunk;
 mod compiler;
 mod scanner;
 
+fn repl() {
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let tokens = scanner::scan(&input);
+        let compiled_chunk: Chunk = compiler::compile(&tokens);
+        compiled_chunk.disassemble("test");
+    }
+}
+
 fn main() {
-    let input = "1+1-1/1*1";
-    let tokens = scanner::scan(input);
-    let compiled_chunk: Chunk = compiler::compile(&tokens);
-    compiled_chunk.disassemble("test");
+    repl();
 }
